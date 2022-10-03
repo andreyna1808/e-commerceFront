27ba96c85daa59c6d8fc346810b821376f9092f3
andreyna1808 <andreyna.m.carvalho@gmail.com>
2022-10-03 16:34:23 -0400

    feat: refatoring axios

--- e-commerce/src/utils/api/user.ts
+++ e-commerce/src/utils/api/user.ts
@@ -4,13 +4,14 @@ import { BASE_URL } from "../constants"
 export const RegisterAPI = async (formData) => {
   try {
     const url = `${BASE_URL}/api/auth/local/register`
+    const body = JSON.stringify(formData)
     const params = {
       headers: {
         "Content-Type": 'application/json'
       },
     };
 
-    const response = await axios.post(url, JSON.stringify(formData), params)
+    const response = await axios.post(url, body, params)
     const result = await response.data
 
     console.log('Awqqqqq', result)
